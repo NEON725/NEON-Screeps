ba6b70ccf69943bbf16577ef57b50dcf593102a8
NEON725 <NEON725@hotmail.com>
2021-10-23 16:31:43 -0500

    ESLint Configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,5 @@
 module.exports = {
+  "root": true,
   env: {
     browser: true,
     es6: true,
@@ -8,8 +9,6 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
-    "plugin:prettier/recommended",
-    "prettier/@typescript-eslint",
     "plugin:import/errors",
     "plugin:import/warnings",
     "plugin:import/typescript"
@@ -19,7 +18,10 @@ module.exports = {
     project: "tsconfig.json",
     sourceType: "module"
   },
-  plugins: ["@typescript-eslint", "import"],
+  plugins: [
+		"@typescript-eslint",
+		"import",
+	],
   settings: {
     "import/parsers": {
       "@typescript-eslint/parser": [".ts", ".tsx"]
@@ -33,30 +35,28 @@ module.exports = {
     "@typescript-eslint/consistent-type-assertions": "error",
     "@typescript-eslint/consistent-type-definitions": "error",
     "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/explicit-member-accessibility": [
-      "error",
-      {
-        accessibility: "explicit"
-      }
-    ],
+    "@typescript-eslint/explicit-member-accessibility": "off",
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-parameter-properties": "off",
     "@typescript-eslint/no-unused-expressions": "error",
     "@typescript-eslint/no-use-before-define": ["error", { functions: false }],
     "@typescript-eslint/prefer-for-of": "error",
-    "@typescript-eslint/space-within-parens": ["off", "never"],
+		"@typescript-eslint/explicit-module-boundary-types": "error",
+		"@typescript-eslint/no-empty-function": "warn",
+		"@typescript-eslint/no-unsafe-member-access": "off",
+    "space-in-parens": ["error", "never"],
     "@typescript-eslint/unified-signatures": "error",
     "arrow-parens": ["off", "as-needed"],
     camelcase: "error",
     complexity: "off",
     "dot-notation": "error",
-    "eol-last": "off",
+    "eol-last": ["error", "never"],
     eqeqeq: ["error", "smart"],
     "guard-for-in": "off",
     "id-blacklist": ["error", "any", "Number", "number", "String", "string", "Boolean", "boolean", "Undefined"],
     "id-match": "error",
     "linebreak-style": "off",
-    "max-classes-per-file": ["error", 1],
+    "max-classes-per-file": ["error", 3],
     "new-parens": "off",
     "newline-per-chained-call": "off",
     "no-bitwise": "error",
@@ -74,7 +74,7 @@ module.exports = {
       }
     ],
     "no-throw-literal": "error",
-    "no-trailing-spaces": "off",
+    "no-trailing-spaces": "error",
     "no-undef-init": "error",
     "no-underscore-dangle": "warn",
     "no-var": "error",
@@ -82,7 +82,13 @@ module.exports = {
     "one-var": ["error", "never"],
     "quote-props": "off",
     radix: "error",
-    "sort-imports": "warn",
+    "sort-imports": "off",
     "spaced-comment": "error",
+    "indent": ["error", "tab"],
+    "brace-style": ["error", "allman", {"allowSingleLine": true}],
+    "@typescript-eslint/no-namespace": "off",
+		"linebreak-style": ["error", "unix"],
+		"space-infix-ops": ["error"],
+		"quotes": ["error", "double"],
   }
 };
