7aba7bb5cc21f66891a0d6f25ac9a5a75f7763a6
NEON725 <NEON725@hotmail.com>
2021-10-27 01:27:33 -0500

    Suppressed warnings from rollup. Linted rollup.config.js.

--- rollup.config.js
+++ rollup.config.js
@@ -1,34 +1,42 @@
 "use strict";
 
-import clear from 'rollup-plugin-clear';
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import typescript from 'rollup-plugin-typescript2';
-import screeps from 'rollup-plugin-screeps';
-import json from '@rollup/plugin-json';
+import clear from "rollup-plugin-clear";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "rollup-plugin-typescript2";
+import screeps from "rollup-plugin-screeps";
+import json from "@rollup/plugin-json";
 
 let cfg;
 const dest = process.env.DEST;
-if (!dest) {
-  console.log("No destination specified - code will be compiled but not uploaded");
-} else if ((cfg = require("./screeps.json")[dest]) == null) {
-  throw new Error("Invalid upload destination");
+if(!dest)
+{
+	console.log("No destination specified - code will be compiled but not uploaded");
+}
+else if((cfg = require("./screeps.json")[dest]) == null)
+{
+	throw new Error("Invalid upload destination");
 }
 
-export default {
-  input: "src/main.ts",
-  output: {
-    file: "dist/main.js",
-    format: "cjs",
-    sourcemap: true
-  },
+let warnings = [];
 
-  plugins: [
-    clear({ targets: ["dist"] }),
-    resolve({ rootDir: "src" }),
-    commonjs(),
-    typescript({tsconfig: "./tsconfig.json"}),
-    screeps({config: cfg, dryRun: cfg == null}),
-		json({ compact: true }),
-  ]
+export default{
+	input: "src/main.ts",
+	output: {
+		file: "dist/main.js",
+		format: "cjs",
+		sourcemap: true,
+	},
+
+	plugins: [
+		clear({targets: ["dist"]}),
+		resolve({rootDir: "src"}),
+		commonjs(),
+		typescript({tsconfig: "./tsconfig.json"}),
+		screeps({config: cfg, dryRun: cfg == null}),
+		json({compact: true}),
+	],
+
+	"//": "Ignore rollup warnings because I don't know how to exclude node_modules.",
+	onwarn: warning => {},
 }
